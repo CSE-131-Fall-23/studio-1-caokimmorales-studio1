212c992c6ae676d2b9a511e6037587d9bde8c2dc
seank <seank@Sean.dhcp.wustl.edu>
2023-09-06 15:19:58 -0500

    Studio 1

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,23 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		
+		boolean isOrdered=true;
+		
+		//Ascending
+		boolean a=(y>x);
+		boolean b=(z>y);
+	
+		
+		//Descending
+		boolean c=(y<x);
+		boolean d=(z<y);
+		
+		isOrdered=a&&b||c&&d;
+		System.out.println(isOrdered);
+		
+		
+		
 
 	}
 
